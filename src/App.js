64a3ab3165d9e92859473771ed0cb8e4d3d15103
dole2711dev <dole2711@gmail.com>
2021-03-01 22:05:25 +0700

    Design website v0.2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import Body from "./components/Body";
@@ -7,9 +8,35 @@ function App() {
 	return (
 		<>
 			<Nav />
-			<Body />
 		</>
 	);
+
+	useEffect(() => {
+		storeScroll();
+	}, [""]);
+
+	const debounce = (fn) => {
+		let frame;
+		return (...params) => {
+			if (frame) {
+				cancelAnimationFrame(frame);
+			}
+
+			frame = requestAnimationFrame(() => {
+				fn(...params);
+			});
+		};
+	};
+
+	const storeScroll = () => {
+		document.documentElement.dataset.scroll = window.scrollY;
+	};
+
+	document.addEventListener("scroll", debounce(storeScroll), {
+		passive: true,
+	});
+
+	storeScroll();
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Nav.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import components
@@ -6,15 +6,21 @@ import InputSearch from "./InputSearch";
 import Home from "./pages/Home";
 import Community from "./pages/Home";
 import Blog from "./pages/Blog";
+import Solutions from "./pages/Solutions";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const Nav = () => {
 	return (
-		<div className="navbar">
-			<div className="navbar-container">
+		<div className="navbar pl-30px">
+			<div className="navbar-left">
 				<div className="br-test">
 					<i className="navbar-icon pr-10px fab fa-asymmetrik"></i>
 					<span className="navbar-title pr-30px">Asymmetrik</span>
 				</div>
+			</div>
+			<div className="navbar-right">
 				<div className="navbar-menu">
 					<Router>
 						<Link to="/home" className="home pl-30px pr-30px">
@@ -28,6 +34,22 @@ const Nav = () => {
 						<Link to="/blog" className="blog pl-30px pr-30px">
 							Blog
 						</Link>
+						<Link
+							to="/solutions"
+							className="solutions pl-30px pr-30px">
+							Solutions
+						</Link>
+						<Link
+							to="/services"
+							className="services pl-30px pr-30px">
+							Services
+						</Link>
+						<Link to="/about" className="about pl-30px pr-30px">
+							About
+						</Link>
+						<Link to="/contact" className="contact pl-30px pr-30px">
+							Contact
+						</Link>
 						<Switch>
 							<Route path="/home">
 								<Home />
@@ -38,18 +60,19 @@ const Nav = () => {
 							<Route path="blog">
 								<Blog />
 							</Route>
+							<Route path="/solutions">
+								<Solutions />
+							</Route>
+							<Route path="/services">
+								<Services />
+							</Route>
+							<Route path="/about">
+								<About />
+							</Route>
+							<Route path="/contact">
+								<Contact />
+							</Route>
 						</Switch>
-					</Router>
-				</div>
-				<InputSearch />
-				<div className="navbar-login">
-					<Router>
-						<Link to="/login" className="btn-login pr-30px">
-							Login<i class="pl-10px fas fa-sign-in-alt"></i>
-						</Link>
-						<Link to="/signin" className="btn-signin">
-							Sign In<i class="pl-10px fas fa-sign-in-alt"></i>
-						</Link>
 					</Router>
 				</div>
 			</div>
